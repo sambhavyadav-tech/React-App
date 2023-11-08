51da2a1ff9414ca9f3b469178bbf58b1fcdc51ec
sambhav yadav <sambhavyadav25@gmail.com>
2023-11-08 11:38:19 +0000

    # add blog page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,37 +3,32 @@ import React from 'react'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const Header = () => {
-   
-    return (
-      <Navbar  expand="lg" className="bg-body-tertiary">
+
+  return (
+    <Navbar bg="light "expand="lg" fixed='top'>
       <Container>
-        <Navbar.Brand href="#home">
-            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="50" viewBox="0 0 200 50">
-              <text x="10" y="40" font-family="Arial, sans-serif" font-size="40" fill="#007bff">bytebuzzio</text>
-            </svg>
+        <Navbar.Brand href="#buzz">
+          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="50" viewBox="0 0 200 50">
+            <text x="10" y="40" font-family="Arial, sans-serif" font-size="40" fill="#007bff">bytebuzzio</text>
+          </svg>
 
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="#buzz">Buzz</Nav.Link>
+            <Nav.Link href="#trends">Trends</Nav.Link>
+            <NavDropdown title="Topics" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Byte Science</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Byte AI</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Byte Tech</NavDropdown.Item>
+
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
-    }
+  )
+}
 
 export default Header
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import Buzz from './Buzz'
+
 
 const Home = () => {
   return (
     <div className='home-container'>
       <Header></Header>
+      <line></line>
+      <Buzz></Buzz>
     </div>
   )
 }
